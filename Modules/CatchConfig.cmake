0a698e8fe8b708d03b3770505a4edb573f8743c2
AlexisWilke <alexis@m2osw.com>
2019-06-10 16:49:20 -0700

    Fixed the FindCatch.cmake, the REQUIRED was not properly tested.

--- Modules/CatchConfig.cmake
+++ Modules/CatchConfig.cmake
@@ -33,20 +33,20 @@
 # CATCH_FOUND           If false, do not try to use Catch.
 #
 
-SET(CATCH_FOUND "NO")
+set(CATCH_FOUND "NO")
 
-FIND_PATH(CATCH_INCLUDE_DIR
+find_path(CATCH_INCLUDE_DIR
         catch.hpp
     PATH_SUFFIXES
         catch
 )
 
-IF(CATCH_INCLUDE_DIR)
-    SET(CATCH_FOUND "YES")
-ELSE(CATCH_INCLUDE_DIR)
-    IF(CATCH_FIND_REQUIRED)
-        MESSAGE(SEND_ERROR "Could not find library Catch.")
-    ENDIF(CATCH_FIND_REQUIRED)
-ENDIF(CATCH_INCLUDE_DIR)
+if(CATCH_INCLUDE_DIR)
+    set(CATCH_FOUND "YES")
+else(CATCH_INCLUDE_DIR)
+    if(Catch_FIND_REQUIRED)
+        message(SEND_ERROR "Could not find library Catch.")
+    endif(Catch_FIND_REQUIRED)
+endif(CATCH_INCLUDE_DIR)
 
 # vim: ts=4 sw=4 et
