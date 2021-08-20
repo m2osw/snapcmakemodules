b96d6951eed3af7efa2e108c03d0821ba67fdb56
AlexisWilke <alexis@m2osw.com>
2021-08-20 16:49:01 -0700

    Added the index.php used by the coverage to the cmake project.
    Fixed location of the cmake_log.txt file.
    Added tests for the cloc statistics (this still needs work).

---
+++ scripts/index.php
@@ -0,0 +1,72 @@
+<?php
+# Display this directory to users
+# (code used to display test results)
+#
+# Copyright (c) 2011-2021  Made to Order Software Corp.  All Rights Reserved
+#
+# https://snapwebsites.org/project/snapcmakemodules
+# contact@m2osw.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+function out($d, $file, $label)
+{
+    $filename = $d . "/" . $file;
+    if(file_exists($filename))
+    {
+        echo "<td><a href=\"", $d, "/", $file, "\">", $label, "</a></td>";
+    }
+    else
+    {
+        echo "<td>&mdash;</td>";
+    }
+}
+
+
+$dir = glob("@PROJECT_NAME@*");
+
+echo "<html><head><title>@PROJECT_NAME@ coverage</title></head>",
+    "<body><h1>@PROJECT_NAME@ coverage</h1>",
+    "<table border=\"1\" cellpadding=\"10\" cellspacing=\"0\">",
+    "<thead><tr>",
+        "<th>Coverage</th>",
+        "<th>CMake</th>",
+        "<th>Build</th>",
+        "<th>Test</th>",
+        "<th>Statistics</th>",
+    "</tr><tbody>";
+
+foreach($dir as $d)
+{
+    echo "<tr>";
+
+        echo "<td><a href=\"", $d, "\">", $d, "</a></td>";
+
+        out($d, "cmake_log.html", "log");
+        out($d, "build_log.html", "log");
+        out($d, "test_log.html", "log");
+        out($d, "statistics.html", "stats");
+
+    echo "</tr>";
+}
+
+echo "</tbody></table></body></html>";
+
+# vim: ts=4 sw=4 et
