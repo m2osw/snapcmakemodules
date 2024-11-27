b989c1a15a111ab0a7d8c45df55a676a00c641ff
AlexisWilke <alexis@m2osw.com>
2024-11-26 16:10:59 -0800

    Added macro to run our unit tests.
    I'd like our build server to run our unit tests so when a project builds,
    we then run the tests before we create the packages. If the tests fail,
    then we have to go back to them to make them pass. This should force me
    to better keep the code up to date.
    
    Fixed the error detecting the BUILD and dist directories in the mk script.

---
+++ Modules/SnapRunUnitTests.sh
@@ -0,0 +1,137 @@
+#!/bin/bash -e
+#
+# This script runs the `unittest` command for the custom command created
+# by the SnapTestRunnerConfig.cmake module.
+#
+# The parameters to the script are defined by the --help command line option.
+#
+################################################################################
+#
+# Copyright (c) 2024  Made to Order Software Corp.  All Rights Reserved
+#
+# http://snapwebsites.org/
+# contact@m2osw.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+################################################################################
+#
+
+while test -n "$1"
+do
+	case "$1" in
+	"--help"|"-h")
+		echo "Usage: `basename $0` --opts"
+		echo "where --opts are:"
+		echo "  --help | -h                print out this help screen"
+		echo "  --binary-dir <dir>         path to this project top binary directory"
+		echo "  --extra-options <opts>     a set of additional options"
+		echo "  --source-dir <dir>         path to this project top source directory"
+		echo "  --unittest-ran <filename>  name of the dependency to generate on success (usually the PROJECT_NAME)"
+		exit 1
+		;;
+
+	"--binary-dir")
+		shift
+		BINARY_DIR="$1"
+		shift
+		;;
+
+	"--extra-options")
+		shift
+		EXTRA_OPTIONS="$1"
+		shift
+		;;
+
+	"--source-dir")
+		shift
+		SOURCE_DIR="$1"
+		shift
+		;;
+
+	"--unittest-ran")
+		shift
+		UNITTEST_RAN="$1"
+		shift
+		;;
+
+	*)
+		echo "error: unknown command line option \"$1\"."
+		exit 1;
+
+	esac
+done
+
+if test -z "${BINARY_DIR}"
+then
+	echo "error: the --binary-dir option is mandatory and cannot be an empty string."
+	exit 1
+fi
+
+if test -z "${SOURCE_DIR}"
+then
+	echo "error: the --source-dir option is mandatory and cannot be an empty string."
+	exit 1
+fi
+
+if test -z "${UNITTEST_RAN}"
+then
+	echo "error: the --unittest-ran option is mandatory and cannot be an empty string."
+	exit 1
+fi
+
+UNITTEST="tests/unittest"
+if test ! -x "${UNITTEST}"
+then
+	echo "error: no tests/unittest executable found."
+	exit 1
+fi
+
+# make sure the temporary directory exists
+#
+mkdir -p "${BINARY_DIR}/tmp"
+
+# find the distribution directory (where things get installed)
+#
+DIST_DIR="${BINARY_DIR}"
+while ! test -d "${DIST_DIR}/dist"
+do
+	DIST_DIR=`dirname "${DIST_DIR}"`
+	if test -z "${DIST_DIR}" -o "${DIST_DIR}" = "/" -o "${DIST_DIR}" = "." -o -d "${DIST_DIR}/BUILD"
+	then
+		echo "error: distribution directory not found starting from \"${BINARY_DIR}\"."
+		exit 1
+	fi
+done
+DIST_DIR="${DIST_DIR}/dist"
+
+# run all the unit tests
+"${UNITTEST}" \
+	--colour-mode ansi \
+	--tmp-dir "${BINARY_DIR}/tmp" \
+	--binary-dir "${BINARY_DIR}" \
+	--dist-dir "${DIST_DIR}" \
+	--source-dir "${SOURCE_DIR}" \
+	--progress \
+	${EXTRA_OPTIONS} \
+		> tests/unittest.log 2>&1
+
+# it worked, "generate the output" (make target dependency)
+#
+touch "${UNITTEST_RAN}"
---
+++ Modules/SnapTestRunnerConfig.cmake
@@ -0,0 +1,97 @@
+################################################################################
+#
+# Copyright (c) 2024  Made to Order Software Corp.  All Rights Reserved
+#
+# https://snapwebsites.org/
+# contact@m2osw.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+################################################################################
+#
+# File:         Modules/SnapRunUnitTestsConfig.cmake
+# Object:       Run the unit tests of a project.
+#
+
+find_program(RUN_UNITTESTS_SCRIPT SnapRunUnitTests.sh PATHS ${CMAKE_MODULE_PATH})
+
+################################################################################
+# Add a target to run the unit tests of a project
+#
+# Usage: RunUnitTestsTarget(
+#             PROJECT_NAME <name>
+#             PROJECT_TEST_ARGS <arg1> <arg2> ... <argN>
+#             [VERBOSE]
+#        )
+# where: <name> is the name you want to give this target; usually "rununittests"
+#
+# RunUnitTestsTarget() assumes that the unit test executable is named
+# "unittest" and lives under the "${CMAKE_CURRENT_BINARY_DIR}/..." folder.
+#
+function(AddUnitTestsTarget)
+    set(options
+        VERBOSE
+    )
+    set(oneValueArgs
+        PROJECT_NAME
+    )
+    set(multiValueArgs
+        PROJECT_TEST_ARGS
+    )
+    cmake_parse_arguments(ARG
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
+
+    set(EXTRA_OPTIONS "")
+    if(ARG_VERBOSE)
+        set(EXTRA_OPTIONS "${EXTRA_OPTIONS} --verbose")
+    endif()
+
+    project(${ARG_PROJECT_NAME})
+
+    set(UNITTEST_RAN ${PROJECT_BINARY_DIR}/${ARG_PROJECT_NAME})
+
+    add_custom_command(
+        OUTPUT
+            "${UNITTEST_RAN}"
+
+        COMMAND
+            ${RUN_UNITTESTS_SCRIPT}
+                "--binary-dir" "${CMAKE_BINARY_DIR}"
+                "--source-dir" "${CMAKE_SOURCE_DIR}"
+                "--unittest-ran" "${UNITTEST_RAN}"
+                "--extra-options" "${ARG_PROJECT_TEST_ARGS} ${EXTRA_OPTIONS}"
+
+        WORKING_DIRECTORY
+            ${CMAKE_BINARY_DIR}
+
+        DEPENDS
+            unittest
+    )
+
+    add_custom_target(${PROJECT_NAME}
+        DEPENDS
+            ${UNITTEST_RAN}
+    )
+endfunction()
+
+# vim: ts=4 sw=4 et
